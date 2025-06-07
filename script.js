613e5430574ebcaef126854a39f11c7395923c19
emilystewart26 <emilystewart26@hotmail.co.uk>
2025-06-07 18:16:15 +0100

    Added slider

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// Hamburger Menu
 function toggleMenu() {
-    const menu = document.getElementById('hamburger-menu');
-    menu.classList.toggle('hidden');
-  }
+  const menu = document.getElementById("hamburger-menu");
+  menu.classList.toggle("hidden");
+}
+
+// Slider
+const slider = document.getElementById("card-slider");
+
+slider.addEventListener("mouseenter", () => {
+  slider.classList.add("paused");
+});
+
+slider.addEventListener("mouseleave", () => {
+  slider.classList.remove("paused");
+});
+
+const cards = slider.querySelectorAll(".card, .flex-shrink-0");
+
+cards.forEach((card) => {
+  const clone = card.cloneNode(true);
+  slider.appendChild(clone);
+});
